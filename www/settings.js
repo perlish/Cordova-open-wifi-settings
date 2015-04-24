56e27e8f615e4884d2a0abcfae6b1f84175e7049
deefactorial <deefactorial@gmail.com>
2015-04-24 16:47:12 -0700

    add settings

--- www/settings.js
+++ www/settings.js
@@ -7,5 +7,9 @@ NativeSettings.prototype.open = function(onsucess, onfail) {
 	exec(onsucess, onfail, "NativeSettings", "open", []);
 };
 
+NativeSettings.prototype.openSetting = function(settingName, onsucess, onfail) {
+	exec(onsucess, onfail, "NativeSettings", settingName, []);
+};
+
 var NativeSettings = new NativeSettings();
 module.exports = NativeSettings;
